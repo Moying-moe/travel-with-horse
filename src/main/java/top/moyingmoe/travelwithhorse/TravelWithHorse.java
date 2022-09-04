3fc244513457bfe0ba18335c8a210f9d00836aab
Moying-moe <741744469@qq.com>
2022-09-04 11:16:18 +0800

    initial

---
+++ src/main/java/top/moyingmoe/travelwithhorse/TravelWithHorse.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 moying All Rights Reserved.
+ *
+ * Distributed under MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
+
+package top.moyingmoe.travelwithhorse;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TravelWithHorse implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/top/moyingmoe/travelwithhorse/client/TravelWithHorseClient.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2022 moying All Rights Reserved.
+ *
+ * Distributed under MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
+
+package top.moyingmoe.travelwithhorse.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TravelWithHorseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
